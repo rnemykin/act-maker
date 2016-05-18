b05108321665c862bc12d9efd1454de3d4a8015d
RNemykin <nemykinroman@gmail.com>
2016-05-18 12:46:50 +0300

    ~ Act2ActPropertyMapper. separate logic

--- src/main/java/ru/act/service/ActService.groovy
+++ src/main/java/ru/act/service/ActService.groovy
@@ -1,48 +1,31 @@
 package ru.act.service
 
-import com.ibm.icu.text.RuleBasedNumberFormat
 import org.apache.poi.openxml4j.opc.OPCPackage
-import org.apache.poi.openxml4j.opc.PackageAccess
 import org.apache.poi.xwpf.usermodel.XWPFDocument
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.util.StreamUtils
 import org.springframework.util.StringUtils
 import ru.act.common.ValidationException
 import ru.act.model.Act
 import ru.act.model.ActDocument
-
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.time.format.DateTimeFormatterBuilder
-import java.time.format.SignStyle
+import ru.act.model.ActProperty
 import java.time.format.TextStyle
 
-import static java.time.temporal.ChronoField.*
 import static org.springframework.util.StringUtils.isEmpty
 
 @Service
 class ActService {
     private static final Logger log = LoggerFactory.getLogger(ActService.class)
 
-    private static def RU_DATE_FORMATTER = new DateTimeFormatterBuilder()
-            .appendValue(DAY_OF_MONTH, 2)
-            .appendLiteral('.')
-            .appendValue(MONTH_OF_YEAR, 2)
-            .appendLiteral('.')
-            .appendValue(YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
-            .toFormatter()
-
     @Autowired
-    private RussianNameProcessor nameProcessor;
-
+    private Act2ActPropertyMapper act2ActPropertyMapper
 
     ActDocument makeAct(Act act) {
         validate(act)
 
-        def actProperty = buildProperties(act)
+        def actProperty = act2ActPropertyMapper.map(act)
         def docTemplate = ActService.class.getResource("/template.docx")
         XWPFDocument doc = new XWPFDocument(OPCPackage.open(docTemplate.file))
         doc.paragraphs.each {
@@ -119,96 +102,10 @@ class ActService {
         text
     }
 
-    ActProperty buildProperties(Act act) {
-        ActProperty actProperty = new ActProperty();
-        actProperty.userName = nameProcessor.inCaseRod(act.userName)
-        actProperty.shortUserName = getShortName(act.userName)
-
-        actProperty.actDay = act.createDate.dayOfMonth
-        actProperty.actMonth = act.createDate.month.getDisplayName(TextStyle.FULL, Locale.forLanguageTag("ru"))
-        actProperty.actYear = act.createDate.year
-        actProperty.actStartDate = act.createDate.withDayOfMonth(1).format(RU_DATE_FORMATTER)
-        actProperty.actEndDate = act.createDate.withDayOfMonth(act.createDate.toLocalDate().lengthOfMonth()).format(RU_DATE_FORMATTER)
-        actProperty.actNumber = act.actNumber
-
-        actProperty.docNumber = act.docNumber
-        actProperty.docSignYear = act.docSignDate.year
-        actProperty.docSignDate = act.docSignDate.format(RU_DATE_FORMATTER)
-        actProperty.certSerial = act.certSerial
-        actProperty.certNumber = act.certNumber
-
-        actProperty.mainTask = act.mainTask
-        actProperty.mainTaskHours = act.mainTaskHours
-        actProperty.salaryRate = act.salaryRate
-
-        if(act.additionalTask) {
-            actProperty.addTask = act.additionalTask
-            actProperty.salaryRate2 = act.salaryRate.multiply(BigDecimal.valueOf(1.5)).setScale(2)
-            actProperty.addSum = act.additionalTaskHours?.multiply(actProperty.salaryRate2)
-            actProperty.addTaskHours = act.additionalTaskHours
-            actProperty.actAddStartDate = actProperty.actStartDate + '-'
-            actProperty.actAddEndDate = actProperty.actEndDate
-        }
-
-        actProperty.mainSum = act.salaryRate.multiply(act.mainTaskHours)
-        actProperty.allSum = actProperty.mainSum.add(actProperty.addSum ?: BigDecimal.ZERO)
-
-        def numberFormat = new RuleBasedNumberFormat(Locale.forLanguageTag("ru"), RuleBasedNumberFormat.SPELLOUT)
-        actProperty.sumStr = numberFormat.format(actProperty.allSum);
-
-        actProperty
-    }
-
-    String getShortName(String userName) {
-        String[] values = userName.split(' ');
-        String lastName = values.size() > 0 ? values[0] : null
-        String firstName = values.size() > 1 ? values[1] : null
-        String middleName = values.size() > 2 ? values[2] : null
-
-        String.format("%s %s. %s.", lastName, firstName?.charAt(0) ?: '', middleName?.charAt(0) ?: '')
-    }
-
-    byte[] getDocxBytes(XWPFDocument xwpfDocument) {
-        def stream = new ByteArrayOutputStream()
-        xwpfDocument.write(stream)
-        stream.toByteArray()
-    }
-
     String getFileName(Act act) {
         String month = act.getCreateDate().getMonth().getDisplayName(TextStyle.FULL_STANDALONE, Locale.forLanguageTag("ru"))
         String userName = act.getUserName().replaceAll(" ", "_")
         String.format("Акт_%s_%s_%s.docx", month, act.getCreateDate().getYear(), userName)
-    }
-
-    static class ActProperty {
-        String userName
-        String shortUserName
-
-        Integer actDay
-        String actMonth
-        Integer actYear
-        String actStartDate
-        String actEndDate
-        String actAddStartDate
-        String actAddEndDate
-        Integer actNumber
-
-        Integer certSerial;
-        String certNumber;
-        Integer docNumber
-        Integer docSignYear
-        String docSignDate
-
-        BigDecimal salaryRate = BigDecimal.ZERO
-        String mainTask
-        BigDecimal mainTaskHours = BigDecimal.ZERO
-        String addTask
-        BigDecimal addTaskHours
-        BigDecimal salaryRate2
-        BigDecimal mainSum = BigDecimal.ZERO
-        BigDecimal addSum
-        BigDecimal allSum = BigDecimal.ZERO
-        String sumStr
     }
 
 }
